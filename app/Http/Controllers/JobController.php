a8a5056ac5bd577be175e4aea15b7d727456049f
GreenWhite <greenwhitedev@gmail.com>
2018-05-28 12:57:44 +0100

    fixed the jobs function, to  be modified better

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,34 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Application;
+use App\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class JobController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         $data['jobs'] = $this->getJobs();
-        $data['title'] ='jobs';
+        $data['title'] = 'jobs';
         return view('jobs', $data);
     }
 
-    public function getJobs($page=1)
+    public function applied()
     {
-        $jobs = Application::leftJoin('jobs', 'applications.job_id',
-            '=', 'jobs.job_id')->where('resume_id', Auth::user()->user_id)
-            ->select('applications.id', 'jobs.title', 'jobs.country', 'jobs.state', 'jobs.lga', 'jobs.description',
-                'salary_from', 'jobs.experience', 'jobs.close_at','applications.created_at','applications.status')->latest()
-            ->get();
+        $data['jobs'] = $this->getJobs();
+        $data['title'] = 'jobs';
+        return view('jobs', $data);
+    }
 
+    public function getJobs($page = 1)
+    {
+        $jobs = Job::whereDate('close_at', '>=', date('Y-m-d'))->latest()
+            ->get();
         return $jobs;
     }
-    public function getAppliedJobs($page=1)
+
+    public function getAppliedJobs($page = 1)
     {
         $jobs = Application::leftJoin('jobs', 'applications.job_id',
             '=', 'jobs.job_id')->where('resume_id', Auth::user()->user_id)
-            ->select('applications.id', 'jobs.title', 'jobs.country', 'jobs.state', 'jobs.lga', 'jobs.description',
-                'salary_from', 'jobs.experience', 'jobs.close_at','applications.created_at','applications.status')->latest()
+            ->select('applications.id', 'jobs.title', 'jobs.country',
+                'jobs.state', 'jobs.lga', 'jobs.description',
+                'salary_from', 'jobs.experience', 'jobs.close_at',
+                'applications.created_at', 'applications.status')->latest()
             ->get();
 
         return $jobs;
