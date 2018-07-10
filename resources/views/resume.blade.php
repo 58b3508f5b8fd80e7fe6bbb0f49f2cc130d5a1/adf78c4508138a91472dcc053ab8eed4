b3a0db817087c15d90ccaaafd16dc4758de9a0fb
GreenWhite <greenwhitedev@gmail.com>
2018-07-10 11:14:39 +0100

    Debugging

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -5,6 +5,21 @@
     <div class="container">
         @include('partials.resume')
     </div>
+    <div id="resume-modal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content" style="background-color: #fff;">
+                <div class="modal-header" style="background-color:#2c3e50;">
+                    <button type="button" class="close" data-dismiss="modal" style="color: #ffffff;">&times;</button>
+                    <h4 class="modal-title" style="color: #ffffff;"></h4>
+                </div>
+                <div class="modal-body pull-left" id="invite" style="background-color: #f5f5f5; float:left;">
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('scripts')
 
