603c86b124391f1726582b149ace846e1e8895f2
GreenWhite <greenwhitedev@gmail.com>
2018-07-14 10:49:13 +0100

    Added excel/pdf download functionality to datatables

--- resources/views/dashboard/admin/control/jobs.blade.php
+++ resources/views/dashboard/admin/control/jobs.blade.php
@@ -2,41 +2,43 @@
 @extends('layouts.admin.dashboard')
 @section('title',$title)
 @section('styles')
+    <link rel="stylesheet" href="{{asset($public.'/dashboard/css/dataTables.min.css')}}">
+    <link rel="stylesheet" href="{{asset($public.'/dashboard/css/buttons.dataTables.min.css')}}">
 @endsection
 @section('content')
-    <main id="main-container" style="min-height: 192px;">
-        <div class="content">
-            <h2 class="content-heading">Jobs</h2>
-            <div class="block">
-                <div class="block-header block-header-default">
-                    <h3 class="block-title">Jobs Table</h3>
-                    <div class="block-options">
-                        <button type="button" class="btn-block-option" data-toggle="block-option"
-                                data-action="fullscreen_toggle"><i class="si si-size-fullscreen"></i></button>
-                        <button type="button" class="btn-block-option" data-toggle="block-option"
-                                data-action="content_toggle"><i class="si si-arrow-up"></i></button>
-                    </div>
-                </div>
-                <div class="block-content">
-                    <table class="table table-responsive data-table table-hover table-vcenter">
-                        <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th class="d-none d-sm-table-cell">Country</th>
-                            <th class="d-none d-sm-table-cell">State</th>
-                            <th class="d-none d-sm-table-cell">LGA</th>
-                            <th class="d-none d-sm-table-cell">Experience</th>
-                            <th class="d-none d-sm-table-cell">Qualification</th>
-                            <th class="d-none d-sm-table-cell">Created at</th>
-                            <th class="d-none d-sm-table-cell">Updated at</th>
-                        </tr>
-                        </thead>
 
-                    </table>
+    <div class="content">
+        <h2 class="content-heading">Jobs</h2>
+        <div class="block">
+            <div class="block-header block-header-default">
+                <h3 class="block-title">Jobs Table</h3>
+                <div class="block-options">
+                    <button type="button" class="btn-block-option" data-toggle="block-option"
+                            data-action="fullscreen_toggle"><i class="si si-size-fullscreen"></i></button>
+                    <button type="button" class="btn-block-option" data-toggle="block-option"
+                            data-action="content_toggle"><i class="si si-arrow-up"></i></button>
                 </div>
             </div>
+            <div class="block-content">
+                <table class="table table-responsive data-table table-hover table-vcenter">
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th class="d-none d-sm-table-cell">Country</th>
+                        <th class="d-none d-sm-table-cell">State</th>
+                        <th class="d-none d-sm-table-cell">LGA</th>
+                        <th class="d-none d-sm-table-cell">Experience</th>
+                        <th class="d-none d-sm-table-cell">Qualification</th>
+                        <th class="d-none d-sm-table-cell">Created at</th>
+                        <th class="d-none d-sm-table-cell">Updated at</th>
+                    </tr>
+                    </thead>
+
+                </table>
+            </div>
         </div>
-    </main>
+    </div>
+
 
 @endsection
 @section('scripts')
@@ -51,6 +53,7 @@
     <script>
         $(document).ready(function () {
             $('.data-table').DataTable({
+                dom: 'lBfrtip',
                 "processing": true,
                 "serverSide": true,
                 "ajax": {
@@ -67,10 +70,15 @@
                     {data: 'created_at', name: 'created_at'},
                     {data: 'updated_at', name: 'updated_at'}
                 ],
+                buttons: [
+                    'excel', 'pdf', 'print'
+                ],
                 "columnDefs": [{
                     "orderable": false,
                     "targets": [1, 2, 3, 4, 5, 6, 7]
                 }],
+                "aLengthMenu": [[25, 50, 100, 200, -1], [25, 50, 100, 200, "All"]],
+                "iDisplayLength": 25
             });
         });
     </script>
